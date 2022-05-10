ae6effbee2ef24589b9f6e688c3e9176262fcd04
joeelkhoury1989 <joeelkhoury1989@gmail.com>
2022-05-10 17:31:09 +0200

    Updates

---
+++ precache-manifest.c381c834ec8d3e1ff29444eee59a7cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf26cdeea96c5cc6947cc8b6204f08b",
+    "url": "/reduxStart/index.html"
+  },
+  {
+    "revision": "7ed1c4265cc63170a8a2",
+    "url": "/reduxStart/static/css/main.ca6b460a.chunk.css"
+  },
+  {
+    "revision": "4ad107ba0c82c6a61c7f",
+    "url": "/reduxStart/static/js/2.172c624b.chunk.js"
+  },
+  {
+    "revision": "84065a442ca1a5b1116c1509672a96fc",
+    "url": "/reduxStart/static/js/2.172c624b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed1c4265cc63170a8a2",
+    "url": "/reduxStart/static/js/main.8f7a9ca9.chunk.js"
+  },
+  {
+    "revision": "347f6048b9e4a011812a",
+    "url": "/reduxStart/static/js/runtime-main.d68c450b.js"
+  }
+]);
